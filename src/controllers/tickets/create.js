fad2feb5a7328240c6ee2ecf728c4b584817cb27
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-22 15:14:13 -0300

    Criando o objeto de ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import { randomUUID } from "node:crypto"
+
 export function create({ request, response }) {
-  return response.end("Criado com sucesso!")
+  const { equipment, description, user_name } = request.body
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    create_at: new Date(),
+    updated_at: new Date(),
+  }
+
+  return response.end(JSON.stringify(ticket))
 }

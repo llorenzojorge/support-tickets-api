e58fbe90812f85eaf1acf3ef512e9e7c868f62e7
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-27 09:04:45 -0300

    Obtendo dados do ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,3 +1,6 @@
 export function update({request, response, database }) {
-  return response.end("TESTE")
+  const { id } = request.params 
+  const { equipment, description } = request.body
+
+  return response.end()
 }
